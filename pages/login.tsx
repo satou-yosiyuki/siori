e8bfdd57bf630f05ea7f7ba7690edeeacd16c4e0
satou-yosiyuki <stuysyk1027@icloud.com>
2023-10-09 09:03:06 +0900

    ログイン画面のUIを仮作成

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./style.module.scss";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  /**
+   * ログイン処理
+   * @param e
+   */
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+  
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">ログイン</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
