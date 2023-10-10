f257569fd94cd70a04cfce755d92b64a829c37eb
satou-yosiyuki <stuysyk1027@icloud.com>
2023-10-10 21:55:46 +0900

    Add Dockerfile and next.config.js settings

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
+module.exports = {
   output: "standalone",
 };
-
-module.exports = nextConfig;
